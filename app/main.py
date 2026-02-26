d2ff5551a540b22e47bfa5bbb9c513cf10e1f338
Aerex0 <suyash.ranjan.min24@itbhu.ac.in>
2026-02-26 16:09:14 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -36,14 +36,14 @@ def main():
         options = sys.argv[2]
         file = sys.argv[3]
         if options == "-w":
-            with open(file, "rb") as f:
+            with open(file, "r") as f:
                 content = f.read()
                 file_hash = hashlib.sha1(content).hexdigest()
                 file_folder = file_hash[:2]
                 file_name = file_hash[2:]
                 os.makedirs(f".git/objects/{file_folder}", exist_ok=True)
                 with open(f".git/objects/{file_folder}/{file_name}", "wb") as f:
-                    f.write(zlib.compress(f"blob {len(content)}\0{content}".encode("utf-8")))
+                    f.write(zlib.compress(f"blob {len(content)}\0{content}"))
                 sys.stdout.write(file_hash)
 
         
