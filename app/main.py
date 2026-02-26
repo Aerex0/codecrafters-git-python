36ec536a723e4590731d3fc82237e3de548a8d78
Aerex0 <suyash.ranjan.min24@itbhu.ac.in>
2026-02-26 15:43:25 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import zlib
 
 
 def main():
@@ -9,6 +10,10 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     
     command = sys.argv[1]
+    options = sys.argv[2]
+    SHA = sys.argv[3]
+    SHA_FOLDER = SHA[:2]
+    SHA = SHA[2:]
     if command == "init":
         os.mkdir(".git")
         os.mkdir(".git/objects")
@@ -16,6 +21,15 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    elif command == "cat-file":
+        if options == "-p":
+            with open(f".git/objects/{SHA_FOLDER}/{SHA}", "rb") as f:
+                compressed_text = f.read()
+                text = zlib.decompress(compressed_text)
+                text = text.decode("utf-8")
+                size = text.split(b' ')[1].split(b'\0')[0]
+                content = text.split(b' ')[1].split(b'\0')[1]
+                print(content)
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
